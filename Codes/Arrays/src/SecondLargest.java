755aa09b16f8e60465e524060d0f855af35c29de
amitesh08 <aamitesh125@gmail.com>
2023-09-13 14:02:36 +0530

    arrays question

---
+++ Codes/Arrays/src/SecondLargest.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the length of an array :");
+        int[] arr=new int[sc.nextInt()];
+
+        for (int i =0;i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("the array is :"+ Arrays.toString(arr));
+
+        int max=0;
+        int smax=1;
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]>max) {
+                smax=max;
+                max = arr[i];
+
+            }
+        }
+        System.out.println("maximum number is "+max);
+        System.out.println("second maximum number is "+smax);
+    }
+}
