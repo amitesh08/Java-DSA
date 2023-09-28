9d98272c1b7782ff5e80f2f7971be442bede0794
amitesh08 <aamitesh125@gmail.com>
2023-09-28 21:52:09 +0530

    rightrotatekstep #bruteforcec arrayquestion 28sep23

---
+++ Codes/Arrays/src/RightRotatebyKstep.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class RightRotatebyKstep {
+    public static void main(String[] args) {
+        //Ques: Rotate the Array to the right by k steps.
+        //#BRUTEFORCE
+        int[] arr = {1,2,3,4,5,6,7};
+        int n = arr.length;
+        int k = 3;
+        System.out.println("Array before Rotation : "+ Arrays.toString(arr));
+
+        int[] ans = rotateArray(arr,n,k);
+        System.out.println("Array after rotating by k step : "+Arrays.toString(ans));
+
+
+    }
+
+    static int[] rotateArray(int[] arr , int n, int k) {
+//        k = k % n;
+        int[] temp = new int[k];
+        for (int i =n-k; i<n; i++) {
+            temp[i-n+k] = arr[i];
+        }
+
+        for (int i = k; i >=0; i--) {
+            arr[i+k] =arr[i];
+        }
+        for (int i = 0; i<k; i++) {
+            arr[i] = temp[i];
+        }
+        return arr;
+
+    }
+}
